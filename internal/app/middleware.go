7222357cd89d6892574cff45fa124c64ad2b4543
Zander Work <zander@zanderwork.com>
2021-12-19 18:33:23 -0500

    disabled logging for /healthcheck

--- internal/app/middleware.go
+++ internal/app/middleware.go
@@ -9,6 +9,11 @@ import (
 // based on the gorilla/mux docs
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// don't log healthcheck b/c i don't care
+		if r.RequestURI == "/healtcheck" {
+			return
+		}
+
 		log.Printf("request from %s to %s\n", r.RemoteAddr, r.RequestURI)
 
 		next.ServeHTTP(w, r)
