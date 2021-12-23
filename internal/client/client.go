f5b30ca6ecc96fcdde2d042ccca8545f33e55695
Zander Work <zander@zanderwork.com>
2021-12-23 08:25:57 -0800

    improved cli output

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,8 +1,11 @@
 package client
 
 import (
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/captainGeech42/hotline/internal/config"
@@ -30,7 +33,9 @@ func getCallbackName(cbName string, cfg *config.Config) (string, string, bool) {
 		log.Panicln(err)
 	}
 
-	log.Println(newCbResp.Message)
+	if newCbResp.Error {
+		log.Printf("got an error from the server when registering callback: %s\n", newCbResp.Message)
+	}
 
 	return newCbResp.Name, newCbResp.FullCbDomain, newCbResp.UsedExisting
 }
@@ -55,6 +60,66 @@ func getRequests(cbName string, since time.Time, cfg *config.Config) (*[]schema.
 	return &resp.HttpRequests, &resp.DnsRequests
 }
 
+func printTime(ts time.Time) {
+	t := ts.Local()
+	fmt.Printf("%d/%02d/%02d %02d:%02d:%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+}
+
+// pretty print a DNS request
+func printDnsRequest(req schema.CbDnsRequest) {
+	printTime(req.Timestamp)
+
+	fmt.Printf("\t\033[1mNew DNS Query\033[0m\n\n\t Source IP:\t%s\n\tQuery Type:\t%s\n\tQuery Name:\t%s\n\n\n", req.SourceIP, req.QueryType, req.QueryName)
+}
+
+// https://gist.github.com/kennwhite/306317d81ab4a885a965e25aa835b8ef
+func wrapStringOnWord(text string, lineWidth int) string {
+	words := strings.Fields(strings.TrimSpace(text))
+	if len(words) == 0 {
+		return text
+	}
+	wrapped := words[0]
+	spaceLeft := lineWidth - len(wrapped)
+	for _, word := range words[1:] {
+		if len(word)+1 > spaceLeft {
+			wrapped += "\n                        " + word
+			spaceLeft = lineWidth - len(word)
+		} else {
+			wrapped += " " + word
+			spaceLeft -= 1 + len(word)
+		}
+	}
+
+	return wrapped
+
+}
+
+// pretty print a HTTP request
+func printHttpRequest(req schema.CbHttpRequest) {
+	printTime(req.Timestamp)
+
+	headerBytes, err := base64.StdEncoding.DecodeString(req.Headers)
+	if err != nil {
+		log.Println("Failed to decode header string")
+		log.Panicln(err)
+	}
+
+	headerStr := string(headerBytes)
+	headerStr = strings.ReplaceAll(headerStr, "\n", "\n                        ")
+	// headerStr = strings.TrimSuffix(headerStr, "\n")
+
+	bodyBytes, err := base64.StdEncoding.DecodeString(req.Body)
+	if err != nil {
+		log.Println("Failed to decode body string")
+		log.Panicln(err)
+	}
+
+	bodyStr := string(bodyBytes)
+	bodyStr = wrapStringOnWord(bodyStr, 50)
+
+	fmt.Printf("\t\033[1mNew HTTP Request\033[0m\n\n\t Source IP:\t%s\n\t   Request:\t%s %s\n\t      Host:\t%s\n\n\t   Headers:\t%s\n\t      Body:\t%s\n\n\n", req.SourceIP, req.Method, req.URI, req.Host, headerStr, bodyStr)
+}
+
 // run the hotline client
 // prefCbName: the preferred callback name from the cli args
 // showHistorical: true if client wants all callbacks for an existing cb logged
@@ -65,10 +130,15 @@ func StartClient(prefCbname string, showHistorical bool, cfg *config.Config) {
 	if usedExisting {
 		log.Printf("Hotline is now using your previous callback: %s\n", cbName)
 	} else {
+		if prefCbname != "" {
+			log.Printf("Your specified callback wasn't found, a new one has been generated instead")
+		}
 		log.Printf("Hotline is now active using your new callback: %s\n", cbName)
 	}
 
-	log.Printf("Start making requests!\n\n\t\t$ curl http://%[1]s/test\n\n\t\t$ dig +short TXT %[1]s\n\n", cbDomain)
+	log.Printf("Start making requests!\n\n\t$ curl http://%[1]s\n\n\t$ dig +short TXT %[1]s\n\n", cbDomain)
+	fmt.Println("===========================================================================")
+	fmt.Println("")
 
 	for {
 		since := time.Now()
@@ -77,11 +147,13 @@ func StartClient(prefCbname string, showHistorical bool, cfg *config.Config) {
 		httpReqs, dnsReqs := getRequests(cbName, since, cfg)
 
 		for _, req := range *dnsReqs {
-			log.Printf("DNS Request: %s request for %s from %s\n", req.QueryType, req.QueryName, req.SourceIP)
+			//log.Printf("DNS Request: %s request for %s from %s\n", req.QueryType, req.QueryName, req.SourceIP)
+			printDnsRequest(req)
 		}
 
 		for _, req := range *httpReqs {
-			log.Printf("HTTP Request: %s request for %s from %s\n", req.Method, req.URI, req.SourceIP)
+			//log.Printf("HTTP Request: %s request for %s from %s\n", req.Method, req.URI, req.SourceIP)
+			printHttpRequest(req)
 		}
 	}
 }
